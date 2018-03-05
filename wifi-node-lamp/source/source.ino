703b14ccf9a44348f64f0f0746983f76497d0412
tqbdev <tranquocbao3897@gmail.com>
2018-03-05 20:04:12 +0700

    Turn off ESP8266 AP set to STA mode

--- wifi-node-lamp/source/source.ino
+++ wifi-node-lamp/source/source.ino
@@ -58,6 +58,7 @@ void setup_wifi()
 {
   delay(10);
   // We start by connecting to a WiFi network
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) 
   {
